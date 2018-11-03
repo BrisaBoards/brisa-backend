87766ef8ce78364d11267c16b444e84ea11f83f0
Andrew <andrew@brisaboards.com>
2018-11-02 23:58:14 -0700

    Add db create/migrate commands to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ source ./environment
 
 echo "Creating a master.key and credentials.yml.enc file..."
 echo ':q' | EDITOR=/bin/true RAILS_ENV=test rails credentials:edit
+rake db:create
+rake db:migrate
