aa5cef621e9d7ec1386da182594b409223688184
Andrew <andrew@brisaboards.com>
2019-01-16 18:27:45 -0800

    Allow setting cable cors in env file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  if ENV['CABLE_ORIGIN']
+    config.action_cable.allowed_request_origins = [ ENV['CABLE_ORIGIN'] ]
+  end
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
